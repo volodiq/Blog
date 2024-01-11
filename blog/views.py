105339396c7b715c825c97c34167b93bff785d0c
rxskee <rxskee@gmail.com>
2024-01-11 15:30:09 +0300

    Создан базовый шаблон

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 
+def index(request):
+    user_is_author = request.user.is_superuser
+    return render(request, 'base.html', context={'user_is_author': user_is_author})
